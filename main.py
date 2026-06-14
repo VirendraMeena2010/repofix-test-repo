9430c6b4d5c455be1b890212ee830678ac6f594f
Virendra Meena <meenavirendra2010@gmail.com>
2026-06-14 11:37:40 +0530

    Implement main function to run math utilities

---
+++ main.py
@@ -0,0 +1,9 @@
+from math_utils import add, divide, is_even
+
+def run():
+    print("Add:", add(5, 3))
+    print("Divide:", divide(10, 2))
+    print("Is Even:", is_even(4))
+
+if __name__ == "__main__":
+    run()
